06693f21492fc5d9ead0772d2efc29ef9a02c33a
justame <justame@gmail.com>
2014-05-12 01:55:34 +0300

    remove

--- www/scripts/controllers/MainCtrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('feedFilter')
-  .controller('MainCtrl', function($scope){
-    $scope.items = ['Phonegap', 'Angular', 'Yeoman'];
-  });
--- www/scripts/init.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-function onDeviceReady() {
-  if (parseFloat(window.device.version) === 7) {
-    document.body.className = document.body.className + ' phone-toolbar-spacing';
-  }
-}
-document.addEventListener('deviceready', onDeviceReady, false);
-angular.bootstrap(document.getElementsByTagName('body')[0], ['feedFilter']);
